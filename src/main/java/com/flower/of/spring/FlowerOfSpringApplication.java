44f69057aa168d88094ed8d13f6b2c10372b6ab6
estu <estudwiyanto21@gmail.com>
2024-06-03 22:58:39 +0700

    feat(garden): A Spring in One's Step ☘️

---
+++ src/main/java/com/flower/of/spring/FlowerOfSpringApplication.java
@@ -0,0 +1,13 @@
+package com.flower.of.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowerOfSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowerOfSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flower/of/spring/FlowerOfSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flower.of.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowerOfSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
